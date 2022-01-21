80edf3d220080dfa3424a9dbe8835436ce1d75df
Peng Shen <sp100809@gmail.com>
2022-01-22 03:18:50 +1100

    firebase-functions

---
+++ typescript.js
@@ -0,0 +1,11 @@
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(1, 1));
+var isBool = true;
+var test = 'test';
+var basket;
+basket = ['basketball', 5];
+var error = function () {
+    throw Error('ops');
+};
---
+++ typescript.ts
@@ -0,0 +1,16 @@
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(1, 1));
+
+
+let isBool: boolean = true;
+
+let test: string = 'test';
+
+let basket: [string, number];
+basket = ['basketball', 5];
+
+let error = (): string => {
+    throw Error('ops');
+}
